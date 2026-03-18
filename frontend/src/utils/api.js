272baad187e81b8ebd9bad4096619bfc7c850025
AtharvaVavhal <atharvavavhal@gmail.com>
2026-03-18 12:23:46 +0530

    feat: update API URL to Railway backend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:8000";
+const API_BASE = "https://caring-charm-production-41d0.up.railway.app";
 
 export const analyzeText = async (text) => {
   const response = await axios.post(`${API_BASE}/api/analyze`, { text });
